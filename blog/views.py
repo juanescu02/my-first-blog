e264c1a8dc61270a219ccf490322fa4e9a77bf55
juanescu02 <juanescu02@gmail.com>
2022-12-16 16:27:49 -0300

    Templates modificados para mostrar post desde base de datos.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.form import UserCreationForm
+from django.views.generic import Created View
+from .models import Post
+from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
